ec4d569f093bee1146b9dab22942f7be65c0308b
Stephany Flores <106363593+sflores926@users.noreply.github.com>
2022-09-27 20:01:02 -0400

    created profile.js

---
+++ models/Profile.js
@@ -0,0 +1,126 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Profile extends Model { }
+
+Profile.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        animal_type:{
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+
+        },
+        age: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        breed_mix: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [3, 100],
+            }
+
+        },
+        personality_quirks: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [3, 200],
+            }
+
+        },
+        kids: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false,
+        },
+        dog: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false,
+
+        },
+        cat: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false,
+
+        },
+        furry_family: {
+            type: DataTypes.STRING,
+            allowNull: true,
+
+        },
+        date_fostered: {
+            type: DataTypes.DATE,
+            allowNull: true,
+            defaultValue: DataTypes.NOW,
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: true,
+            unique: true,
+            validate: {
+                isEmail: true,
+            },
+        },
+        diet: {
+            type: DataTypes.STRING,
+            validate: {
+                len: [3, 400],
+            }
+
+        },
+        neutered_spayed: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false,
+
+        },
+        vaxxed: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false,
+
+        },
+        i_love: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [3, 400],
+            }
+
+        },
+        adopt_me: {
+            type: DataTypes.STRING,
+            allowNull: true,
+            validate: {
+                isUrl: true,
+            }
+
+
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'users',
+                key: 'id',
+            },
+        },
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'profile',
+    }
+);
+
+module.exports = Profile;
