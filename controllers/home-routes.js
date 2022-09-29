0c1680b731c1c1f2fef3151e36ecc79b29f6f8f2
Greg <litm47@gmail.com>
2022-09-29 19:54:59 -0400

    merging process

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,8 +1,18 @@
 const router = require('express').Router();
 
-const { Profile, Users} = require('../models');
+const { Profile, Users, Post } = require('../models');
 
-
+router.get('/profile/:id', async (req, res) => {
+    try {
+      const profile_postId = await Users.findByPk(req.params.id, {
+        attributes: { exclude: ['password']},
+        include: [{ model: Post },{ model: Profile }],
+      });
+      res.status(200).json(profile_postId);
+    } catch (err) {
+      res.status(400).json(err); 
+    }
+  });
 
 
 
