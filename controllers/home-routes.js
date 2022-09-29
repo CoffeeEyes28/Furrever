d6da994077a2abd6357f7f783c2affe99316e764
monica dolce <106848108+monicadolce@users.noreply.github.com>
2022-09-29 15:33:17 -0400

    edited home-routes.js with working/not working get routes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -32,7 +32,7 @@ router.get('/', async (req, res) => {
   }
 });
 
-// Alternative (original)
+// Alternative (original and working in insomnia)
 // router.get('/', (req, res) => {
 //   Profile.findAll({
 //     attributes: {
@@ -59,139 +59,153 @@ router.get('/', async (req, res) => {
 //   });
 // });
 
-
-
-router.get('/profile', async (req, res) => {
+// Working
+router.get('/profile/:user_id', async (req, res) => {
   try {
-    const userData = await Profile.findByPk(req.user_id, {
-      attributes: { exclude: ['password'] },
-      include: [{ model: Post,
-       attributes: [
-        'id', 'media', 'caption',
-       ]}],
+    const profile_postId = await Users.findByPk(req.params.user_id, {
+      attributes: { exclude: ['password']},
+      include: [{ model: Post },{ model: Profile }],
     });
-
-    const user = userData.get({ plain: true });
-
-    res.json('profile', {
-      ...user,
-  
-    });
+    res.status(200).json(profile_postId);
   } catch (err) {
-    res.status(500).json(err);
+    res.status(400).json(err); 
   }
 });
 
-
-router.get('/profile/:id', (req, res) => {
-  Profile.findByPk(req.params.id).then((profileData) => {
-    res.json(profileData);
-  });
-});
-
-router.get('/login', (req, res) => {
-  // If the user is already logged in, redirect to the homepage
-  if (req.session.loggedIn) {
-    res.redirect('/');
-    return;
-  }
-  // Otherwise, render the 'login' template
-  res.render('login');
-});
-
-module.exports = router;
-
-
-
-
-// router.get('/login', (req, res) => {
-//   Profile.findAll().then((profileData) => {
+// Working in insomnia
+// router.get('/profile/:id', (req, res) => {
+//   Profile.findByPk(req.params.id).then((profileData) => {
 //     res.json(profileData);
 //   });
 // });
 
-// router.get('/', (req, res) => {
-//   // TODO: Add a comment describing the functionality of this method
-//   Profile.findAll().then((profileData) => {
-//     res.json(profileData);
+// Working in insomnia
+// router.get('/user/:id', (req, res) => {
+//   Users.findByPk(req.params.id).then((userData) => {
+//     res.json(userData);
 //   });
 // });
 
-// router.get('/', async (req, res) => {
+// Working in insomnia
+// router.get('/post/:id', (req, res) => {
+//   Post.findByPk(req.params.id).then((postData) => {
+//     res.json(postData);
+//   });
+// });
+
+// Working (adds user 2 to complete profile of user 1)
+// router.get('/:id', async (req, res) => {
 //   try {
-//     // Get all users and JOIN with user data
-//     const usersData = await Users.findAll({
-//       include: [
-//         {
-//           model: Users,
-//           attributes: ['name', 'picture'],
-//         },
-//       ],
+//     const profile_userId = await Profile.findByPk(req.params.id, {
+//       include: [{ model: Users }],
 //     });
-
-//     // Serialize data so the template can read it
-//     const users = userstData.map((users) => users.get({ plain: true }));
-
-//     // Pass serialized data and session flag into template
-//     res.render('home', { 
-//       users, 
-//       logged_in: req.session.logged_in 
-//     });
+//     res.status(200).json(profile_userId);
 //   } catch (err) {
-//     res.status(500).json(err);
+//     res.status(400).json(err); 
 //   }
 // });
 
+// Working
+// router.get('/:id', async (req, res) => {
+//   try {
+//     const user_postId = await Users.findByPk(req.params.id, {
+//       include: [{ model: Post }],
+//     });
+//     res.status(200).json(user_postId);
+//   } catch (err) {
+//     res.status(400).json(err); 
+//   }
+// });
+
+// Working
+// router.get('/:id', async (req, res) => {
+//   try {
+//     const user_profileId = await Users.findByPk(req.params.id, {
+//       include: [{ model: Profile }],
+//     });
+//     res.status(200).json(user_profileId);
+//   } catch (err) {
+//     res.status(400).json(err); 
+//   }
+// });
+
+// Working but wrong pet to user
+// router.get('/:id', async (req, res) => {
+//   try {
+//     const post_userId = await Post.findByPk(req.params.id, {
+//       include: [{ model: Users }],
+//     });
+//     res.status(200).json(post_userId);
+//   } catch (err) {
+//     res.status(400).json(err); 
+//   }
+// });
+
+// Not Working
+// router.get('/:id', async (req, res) => {
+//   try {
+//     const post_profileId = await Post.findByPk(req.params.id, {
+//       include: [{ model: Profile }],
+//     });
+//     res.status(200).json(post_profileId);
+//   } catch (err) {
+//     res.status(400).json(err); 
+//   }
+// });
+
+// Not working
 // router.get('/profile/:id', async (req, res) => {
 //   try {
-//     const usersData = await Users.findByPk(req.params.id, {
+//     const profileData = await Profile.findByPk(req.params.id, {
 //       include: [
-//         {
-//           model: Users,
-//           attributes: ['name', 'picture'],
-//         },
-//       ],
+//         { 
+//           model: Post,
+//           attributes: [
+//             'id',
+//             'media',
+//             'caption',
+//             'user_id',
+//           ]
+//         }],
 //     });
-
-//     const users = userstData.get({ plain: true });
-
-//     res.render('users', {
-//       ...profile,
-//       logged_in: req.session.logged_in
-//     });
+//     res.status(200).json(profileData);
 //   } catch (err) {
-//     res.status(500).json(err);
+//     res.status(400).json(err); 
 //   }
 // });
 
-// Use withAuth middleware to prevent access to route
-// router.get('/profile', withAuth, async (req, res) => {
+// Not working
+// router.get('/profile', async (req, res) => {
 //   try {
-//     // Find the logged in user based on the session ID
-//     const userData = await User.findByPk(req.session.user_id, {
+//     const userData = await Profile.findByPk(req.user_id, {
 //       attributes: { exclude: ['password'] },
-//       include: [{ model: Project }],
+//       include: [{ model: Post,
+//        attributes: [
+//         'id', 'media', 'caption',
+//        ]}],
 //     });
 
 //     const user = userData.get({ plain: true });
 
-//     res.render('profile', {
+//     res.json('profile', {
 //       ...user,
-//       logged_in: true
+  
 //     });
 //   } catch (err) {
 //     res.status(500).json(err);
 //   }
 // });
 
+// Not working
 // router.get('/login', (req, res) => {
-//   // If the user is already logged in, redirect the request to another route
-//   if (req.session.logged_in) {
-//     res.redirect('/profile');
+//   // If the user is already logged in, redirect to the homepage
+//   if (req.session.loggedIn) {
+//     res.redirect('/');
 //     return;
 //   }
-
+//   // Otherwise, render the 'login' template
 //   res.render('login');
 // });
 
 module.exports = router;
+
