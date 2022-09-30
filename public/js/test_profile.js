d5d1d2f7bdbfd0c845108efb4216306f28209028
Ruth Kim <ruthkim@Ruths-MacBook-Air.local>
2022-09-30 19:38:17 -0400

    diet, email and i_love not coming in as strings

--- public/js/test_profile.js
+++ public/js/test_profile.js
@@ -1,51 +1,114 @@
-// 1. Testing Phase One on questionnaire first
-var questionnaire = [
-    $(".animal_type"),
-    $("#name"),
-    $("#age"),
-    $("#breed_mix"),
-    $("#personality_quirks"),
-    $(".dog"),
-    $(".cat"),
-    $(".kids"),
-    $("#furry_family"),
-    $("#email"),
-    $("#date_fostered"),
-    $(".neutered_spayed"),
-    $(".vaxed"),
-    $("#diet"),
-    $("#i_love")
-]
-// To keep all local storage items after refreshing page
-$(document).ready(function () {
-    $("#create_profile").on("submit", function (event) {
-        event.preventDefault();
-        let savedAnswerValue=[];
-        for (i = 0; i < questionnaire.length; i++) {
-        //    questionnaire[i].first().val();
-           savedAnswerValue.push(questionnaire[i].first().val())
-            // console.log(questionnaire.val);
-        }
-        console.log(savedAnswerValue)
-    })
-})
-// To keep all local storage items after refreshing page
-// $(document).ready(function () {
-//     for (i = 0; i < questionnaire.length; i++) {
-//         let answerValue = i;
-//         var savedAnswerValue = localStorage.getItem(answerValue);
-//         console.log(savedAnswerValue);
-//         questionnaire[i].val(savedAnswerValue);
-//     }
-// })
+// Object I want rendered
+document.getElementById("create_profile").addEventListener("click", function(event){
 
-// 2. if input type = radio or selection from form....
+// const for field inputs that are strings
+const age = document.getElementById("age").value;
+const breed_mix = document.getElementById("breed_mix").value;
+const personality_quirks = document.getElementById("personality_quirks").value;
+const furry_family = document.getElementById("furry_family").value;
+const date_fostered = document.getElementById("date_fostered").value;
+const email = document.getElementById("email");
+const diet = document.getElementById("diet");
+const i_love = document.getElementById("i_love");
+const adopt_me_url = document.getElementById("adopt_me_url");
 
-// 3. Focusing on URL laters 
-const adopt_me_url = $("#adopt_me_url").val;
-const miscBtn = $("#submit_url")
+// const for animal_type radio buttons
+const animal_type_dog = document.getElementById("animal_type_dog");
+const animal_type_cat = document.getElementById("animal_type_cat");
 
+let animal_type = "";
+    if (animal_type_cat.checked == true){
+        animal_type = "cat"
+    } else if (animal_type_dog.checked == true){
+        animal_type = "dog"
+    } else {
+        console.log("choose a radio button!")
+    }
 
+// const for get along with dogs radio buttons
+const dog_yes = document.getElementById("dog_yes");
+const dog_no = document.getElementById("dog_no")
 
+let dog = "";
+    if (dog_yes.checked == true){
+        dog = "yes"
+    } else if (dog_no.checked == true){
+        dog = "no"
+    } else {
+        console.log("choose a radio button!")
+    }
 
+// const for get along with cats radio buttons
+const cat_yes = document.getElementById("cat_yes");
+const cat_no = document.getElementById("cat_no")
 
+let cat = "";
+    if (cat_yes.checked == true){
+        cat = "yes"
+    } else if (cat_no.checked == true){
+        cat = "no"
+    } else {
+        console.log("choose a radio button!")
+    }
+
+// const for get along with kids radio buttons
+const kids_yes = document.getElementById("cat_yes");
+const kids_no = document.getElementById("cat_no")
+
+let kids = "";
+    if (kids_yes.checked == true){
+        kids = "yes"
+    } else if (kids_no.checked == true){
+        kids = "no"
+    } else {
+        console.log("choose a radio button!")
+    }
+
+// const for get neutered_spayed radio buttons
+const neutered_spayed_yes = document.getElementById("neutered_spayed_yes");
+const neutered_spayed_no = document.getElementById("neutered_spayed_no")
+
+let neutered_spayed = "";
+    if (neutered_spayed_yes.checked == true){
+        neutered_spayed = "yes"
+    } else if (neutered_spayed_no.checked == true){
+        neutered_spayed = "no"
+    } else {
+        console.log("choose a radio button!")
+    }
+
+// const for get neutered_spayed radio buttons
+const vaxed_yes = document.getElementById("vaxed_yes");
+const vaxed_no = document.getElementById("vaxed_no")
+
+let vaxed = "";
+    if (vaxed_yes.checked == true){
+        vaxed = "yes"
+    } else if (vaxed_no.checked == true){
+        vaxed = "no"
+    } else {
+        console.log("choose a radio button!")
+    }
+
+    var questionnaire = {
+        animal_type: animal_type, 
+        age: age, 
+        breed_mix:breed_mix,
+        personality_quirks: personality_quirks,
+        furry_family: furry_family,
+        date_fostered: date_fostered,
+        email: email,
+        diet:diet,
+        kids: kids,
+        dog: dog,
+        cat: cat,
+        neutered_spayed:neutered_spayed,
+        vaxed:  vaxed,
+        i_love: i_love,
+        adopt_me_url: adopt_me_url
+    };
+    
+    event.preventDefault();
+    console.log("button is working!");
+    console.log(questionnaire)
+});
