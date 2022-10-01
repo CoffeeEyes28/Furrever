4c61defd4eb3e02c97bb13f281f07e871bcfe631
Ruth Kim <ruthkim@Ruths-MacBook-Air.local>
2022-10-01 18:35:35 -0400

    profiles2 roughly done with testing

---
+++ public/js/test_profile2.js
@@ -0,0 +1,11 @@
+document.getElementById("create_feed").addEventListener("click", function(event){
+    event.preventDefault();
+    const new_feed = document.getElementById("new_feed").value;
+    const caption = document.getElementById("caption")
+    caption.innerText = new_feed;
+    var posted_feed = {
+        caption: new_feed
+    };
+    console.log("button worked!");
+    console.log(posted_feed)
+});
