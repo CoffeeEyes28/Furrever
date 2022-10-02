061a425ed6e8c58d0f039ccd87c53d7f1104a2ad
Ruth Kim <ruthkim@Ruths-MacBook-Air.local>
2022-10-02 19:52:55 -0400

    testing phase for profiles2 pending to accommodate cloudinary

--- public/js/test_profile2.js
+++ public/js/test_profile2.js
@@ -1,12 +1,24 @@
+
+const photo_feed = document.getElementById("photo_feed").value;
+
 document.getElementById("create_feed").addEventListener("click", function(event){
     event.preventDefault();
+
+   
+    const media = document.getElementById("media").value;
+       
+    
+        
     const new_feed = document.getElementById("new_feed").value;
-    const caption = document.getElementById("caption")
+    const caption = document.getElementById("caption");
+
     caption.innerText = new_feed;
+
     var posted_feed = {
-        media: "not now",
+        media: media,
         caption: new_feed
     };
+
     console.log("button worked!");
     console.log(posted_feed)
 });
