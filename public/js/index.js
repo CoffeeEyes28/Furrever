5c4b3c7b5b6a01f528750789129f7cf61e2e5ab1
monica dolce <106848108+monicadolce@users.noreply.github.com>
2022-10-06 13:26:20 -0400

    updated index.js

--- public/js/index.js
+++ public/js/index.js
@@ -1,9 +1,13 @@
+// The Document method getElementById() returns an Element object 
+// representing the element whose id property matches the specified string.
 const home = document.getElementById('home')
 const login = document.getElementById('login')
 const profile = document.getElementById('profile')
 
 
 
+// The addEventListener() method of the EventTarget interface sets up a 
+// function that will be called whenever the specified event is delivered to the target.
 
 home.addEventListener('click', function (){
     window.location = '/';
