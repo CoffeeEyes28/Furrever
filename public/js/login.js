1cfc57fa545fc5a278fbca55a08974ae2a4bec6b
monica dolce <106848108+monicadolce@users.noreply.github.com>
2022-10-06 13:20:41 -0400

    updated login.js

--- public/js/login.js
+++ public/js/login.js
@@ -1,10 +1,13 @@
 const loginFormHandler = async (event) => {
     event.preventDefault();
   
+    // Collect values from the login form
     const email = document.querySelector('#email-login').value.trim();
     const password = document.querySelector('#password-login').value.trim();
   
     if (email && password) {
+
+       // Send a POST request to the API endpoint
       const response = await fetch('/api/users/login', {
         method: 'POST',
         body: JSON.stringify({ email, password }),
@@ -12,6 +15,7 @@ const loginFormHandler = async (event) => {
       });
   
       if (response.ok) {
+        // If successful, redirect the browser to the homepage 
         document.location.replace('/');
       } else {
         alert('Failed to log in.');
@@ -21,7 +25,12 @@ const loginFormHandler = async (event) => {
   };
   
   
-  
+// The Document method getElementById() returns an Element object representing 
+// the element whose id property matches the specified string.
+
+// The addEventListener() method of the EventTarget interface sets up a function that 
+// will be called whenever the specified event is delivered to the target.
+
 document.getElementById('login-form').addEventListener('submit', loginFormHandler);
 
 
